239ef553a33165397e6c6599a83301393dc50877
Glen Nfor <glennfor72@gmail.com>
2023-12-29 15:20:20 -0500

    chore: setup project dependecies

--- src/wordle/core.cljs
+++ src/wordle/core.cljs
@@ -1,7 +1,10 @@
 (ns wordle.core
-  (:require
-   [reagent.core :as r]
-   [reagent.dom :as d]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [reagent.dom :as rdom]))
+(enable-console-print!)
+
+(defonce app-state (atom {:text "Hello world!"}))
+
 
 ;; -------------------------
 ;; Views
@@ -9,11 +12,13 @@
 (defn home-page []
   [:div [:h2 "Welcome to Reagent"]])
 
+(defn App []
+  [:p "Am I connected?"])
 ;; -------------------------
 ;; Initialize app
 
 (defn mount-root []
-  (d/render [home-page] (.getElementById js/document "app")))
+  (rdom/render [home-page] (.getElementById js/document "app")))
 
 (defn ^:export init! []
   (mount-root))
